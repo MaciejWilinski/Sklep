3ab6f78e0af1cc3c591baa9bfc3c6ad933cedf87
maciek1593829 <144246308+RDG42131@users.noreply.github.com>
2023-10-10 21:12:06 +0200

    Update Sklep.php

--- Sklep.php
+++ Sklep.php
@@ -1,11 +1,52 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Strona logowania</title>
+    <style>
+header {
+    background-color: royalblue;
+    color: #fff;
+    padding: 10px 0;
+    text-align: center;
+}
+
+nav ul {
+    list-style: none;
+    padding: 0;
+}
+
+nav ul li {
+    display: inline;
+    margin-right: 40px;
+}
+
+nav ul li a {
+    text-decoration: none;
+    color: white;
+}
+
+#login-form, #register-form {
+    display: none;
+}
+#logowanie-link{
+    margin-right: 40px;
+}
+    </style>
 </head>
 <body>
-    
+    <header>
+        <h1>Sklep</h1>
+        <nav>
+            <ul>
+                <li><a href="Sklep.php">Strona główna</a></li>
+                <li><a href="#">O nas</a></li>
+                <li><a href="#">Kontakt</a></li>
+                <li id="user-menu">
+                    <a href="Logowanie.php" id="logowanie-link">Zaloguj</a>
+                    <a href="Rejestracja.php">Zarejestruj</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
 </body>
 </html>

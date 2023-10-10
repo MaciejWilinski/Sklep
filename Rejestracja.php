26314d2b15eb80de16bb655b4d0eda5cb580d296
maciek1593829 <144246308+RDG42131@users.noreply.github.com>
2023-10-10 13:39:16 +0200

    Update Rejestracja.php

--- Rejestracja.php
+++ Rejestracja.php
@@ -33,6 +33,7 @@ if(isset($_POST["submit"])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sklep</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 </head>
 <body>
@@ -55,7 +56,7 @@ if(isset($_POST["submit"])){
             <input type="password" name="potwierdzhaslo" id="potwierdzhaslo" require/><br>
 
             <button type="submit" name="submit">Rejestruj</button>
-    </div>
+        </form>
     <p>Masz już konto? <a href="Logowanie.php">Zaloguj sie</p>
 </body>
 </html>
